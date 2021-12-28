e903daf976a516bcad6a5787fed50d7daf1fcd10
Rahul_Das <r.das699@gmail.com>
2021-12-28 22:00:44 +0530

    RGB to Gray Conversion

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,49 @@
-use ndarray::{Array3, Ix3};
-use ndarray_vision::core::*;
-use ndarray_vision::format::netpbm::*;
-use ndarray_vision::format::*;
-use ndarray_vision::processing::*;
+
 use std::path::{Path, PathBuf};
+use std::fs::File;
+use std::io::BufReader;
+
+use jpeg_decoder::{Decoder,PixelFormat};
+use ndarray_vision::core::{RGB, Image, Gray};
 
 fn main() {
     let root = Path::new("C:/Users/admin/Desktop/codespace/python/Structure-from-Motion/dataset/box");
-    let cameraman = root.clone().join("IMG_20200328_172713.jpg");
-    println!("{:?}", cameraman);
+    let test_image = root.clone().join("IMG_20200328_172713.jpg");
+    println!("{:?}", test_image);
+        
+        
+    let file = File::open(test_image).expect("failed to open file");
+    let mut decoder = Decoder::new(BufReader::new(file));
+    let pixels = decoder.decode().expect("failed to decode image");
+    let metadata = decoder.info().unwrap();    
 
-    let decoder = PpmDecoder::default();
-    let image: Image<u8, _> = decoder
-        .decode_file(cameraman)
-        .expect("Couldn't open cameraman.ppm");
+    println!("pixel_format from Metatdata {:?}",metadata.pixel_format);
+    println!("width from Metatdata {:?}",metadata.width);
+    println!("height from Metatdata {:?}",metadata.height);
 
-    let boxkern: Array3<f64> =
-        BoxLinearFilter::build(Ix3(3, 3, 3)).expect("Was unable to construct filter");
+    assert_eq!(
+        metadata.pixel_format,
+        PixelFormat::RGB24,
+        "Colour format is wrong"
+    );
+    let image = Image::<u8, RGB>::from_shape_data(metadata.height as usize, metadata.width as usize, pixels);
+    assert_eq!(
+        metadata.height,
+        image.rows() as u16,
+        "Height  is wrong"
+    );
 
-    let mut image: Image<f64, _> = image.into_type();
+    assert_eq!(
+        metadata.width,
+        image.cols() as u16,
+        "Width  is wrong"
+    );
 
-    let _ = image
-        .conv2d_inplace(boxkern.view())
-        .expect("Poorly sized kernel");
-    // There's no u8: From<f64> so I've done this to hack things
+    println!("Image size {}X{}",image.cols(),image.rows());
 
-    let mut cameraman = PathBuf::from(&root);
-    cameraman.push("images/cameramanblur.ppm");
+    let gray_image=Image::<u8, Gray>::from(image);
 
-    let ppm = PpmEncoder::new_plaintext_encoder();
-    ppm.encode_file(&image, cameraman)
-        .expect("Unable to encode ppm");
+    println!("Gray Images chennal {}",gray_image.channels());
+
+    //let gray_image_path = PathBuf::from("./gray_image.jpg");
 }
